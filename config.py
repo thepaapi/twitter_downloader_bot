5e1371a4e13991ad8af069fc7c353f3e328fee64
thepaapi <bigstarkmail@gmail.com>
2024-08-17 16:52:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-BOT_TOKEN = "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11"  # Your telegram bot token
+BOT_TOKEN = "7468168686:AAHI0BUMjimsoGM3bzEAYB3Hz9jL_aGNoO0"  # Your telegram bot token
 DEVELOPER_ID = 1234567890  # Your telegram id (used for error reporting and private mode)
 IS_BOT_PRIVATE = False  # Change this to True to make bot private
